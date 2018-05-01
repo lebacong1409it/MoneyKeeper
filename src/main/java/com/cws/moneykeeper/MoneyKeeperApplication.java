8dd8bb4aaf5bb464c412de980b17ba7217bafb81
Lê Bá Công <lebacong1409it@gmail.com>
2018-05-01 22:23:21 +0700

    Add jpa to pom.xml and sql to project Edit application.properties and moneykeeper/MoneyKeeperApplication.java

--- src/main/java/com/cws/moneykeeper/MoneyKeeperApplication.java
+++ src/main/java/com/cws/moneykeeper/MoneyKeeperApplication.java
@@ -2,9 +2,14 @@ package com.cws.moneykeeper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class MoneyKeeperApplication {
+@Configuration
+@ComponentScan(basePackages = "com.cws.moneykeeper")
+public class MoneyKeeperApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoneyKeeperApplication.class, args);
